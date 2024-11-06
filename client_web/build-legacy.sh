5e449d7ddc82b823c46c97a49a1653d4c263bffb
Totox00 <bladewielder.roboman@gmail.com>
2024-11-06 04:29:22 +0100

    fixed build-legacy not fetching legacy branch

--- client_web/build-legacy.sh
+++ client_web/build-legacy.sh
@@ -1,8 +1,12 @@
+rm -rf legacy
+rm -rf www/legacy
 mkdir legacy
 mkdir www/legacy
 cd legacy
 git clone https://github.com/Totox00/ap-sotm-client.git
-cd ap-sotm-client/client_web
+cd ap-sotm-client
+git checkout legacy
+cd client_web
 ./build.sh
 cd ../../../
 cp -r legacy/ap-sotm-client/client_web/www/pkg www/legacy
